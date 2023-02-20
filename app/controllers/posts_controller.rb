0d76bfdf504ad30035d0fa880ea25fc50bfec96b
Carlos Rodriguez <ucroxlive@gmail.com>
2023-02-20 03:06:46 -0500

    posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.with_attached_image
+  end
+
+  def show
+    post
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path, notice: t('.created')
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    post
+  end
+
+  def update
+
+    if post.update(post_params)
+      redirect_to posts_path, notice: t('.updated')
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    post.destroy
+
+    redirect_to posts_path, notice: t('.destroyed'), status: :see_other
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:description, :image)
+  end
+
+  def post
+    @post = Post.find(params[:id])
+  end
+end
